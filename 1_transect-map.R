559692ba737496fda30f90aa8cba723757f85623
Stepfanie M Aguillon <38443540+stepfanie-aguillon@users.noreply.github.com>
2022-03-28 11:55:18 -0700

    Update 1_transect-map.R

--- 1_transect-map.R
+++ 1_transect-map.R
@@ -1,11 +1,11 @@
 # This script contains the R code to map sampling localities.
 #
 # Authors: Aguillon SM, VG Rohwer
-# Year: 2021
-# Title: Revisiting a classic hybrid zone: rapid movement of the
+# Year: 2022
+# Title: Revisiting a classic hybrid zone: movement of the
 #        northern flicker hybrid zone in contemporary times
-# Journal Info: TBD
-# bioRxiv DOI: 10.1101/2021.08.16.456504
+# Journal Info: Evolution
+# DOI: 10.1111/evo.14474
 #
 # Edited date: June 2021
 #
