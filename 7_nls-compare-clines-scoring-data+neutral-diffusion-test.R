11dca1be349a96b8ef7ba9ef4597fba9d3928fbd
Stepfanie M Aguillon <38443540+stepfanie-aguillon@users.noreply.github.com>
2022-03-28 11:56:28 -0700

    Update 7_nls-compare-clines-scoring-data+neutral-diffusion-test.R

--- 7_nls-compare-clines-scoring-data+neutral-diffusion-test.R
+++ 7_nls-compare-clines-scoring-data+neutral-diffusion-test.R
@@ -2,11 +2,11 @@
 # HI clines with AIC and test for neutral diffusion.
 #
 # Authors: Aguillon SM, VG Rohwer
-# Year: 2021
-# Title: Revisiting a classic hybrid zone: rapid movement of the
+# Year: 2022
+# Title: Revisiting a classic hybrid zone: movement of the
 #        northern flicker hybrid zone in contemporary times
-# Journal Info: TBD
-# bioRxiv DOI: 10.1101/2021.08.16.456504
+# Journal Info: Evolution
+# DOI: 10.1111/evo.14474
 #
 # Edited date: June 2021
 #
